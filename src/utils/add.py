d8d51fe7c41a3841180e709b52589e2b50f0760a
Sudhanshu Panda <spanda4@hawk.iit.edu>
2024-02-16 18:22:44 -0600

    space fix

--- src/utils/add.py
+++ src/utils/add.py
@@ -1,11 +1,14 @@
+from utils.dbconfig import dbconfig
+import utils.aesutil
 from getpass import getpass
+
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA512
 from Crypto.Random import get_random_bytes
-from utils.dbconfig import dbconfig
-from rich import print as printc
+import base64
 
-import utils.aesutil
+from rich import print as printc
+from rich.console import Console
 
 def computeMasterKey(mp, ds):
     password = mp.encode()
@@ -15,19 +18,19 @@ def computeMasterKey(mp, ds):
 
 
 def addEntry(mp, ds, sitename, siteurl, email, username):
-#get the password
+    #get the password
     password = getpass("Password: ")
 
     mk = computeMasterKey(mp, ds)
 
     encrypted = utils.aesutil.encrpt(key=mk, source=password, KeyType="bytes")
 
-#Add to db
-db = dbconfig()
-cursor =db.cursor()
-query = "INSERT INTO pm.entries (sitename, siteurl, email, username, password) values (%s, %s, %s, %s, %s)"
-val = (sitename, siteurl, email, username, encrypted)
-cursor.execute(query, val)
-db.commit()
+    #Add to db
+    db = dbconfig()
+    cursor =db.cursor()
+    query = "INSERT INTO pm.entries (sitename, siteurl, email, username, password) values (%s, %s, %s, %s, %s)"
+    val = (sitename, siteurl, email, username, encrypted)
+    cursor.execute(query, val)
+    db.commit()
 
 printc("[green][+][/green] Added entry")
