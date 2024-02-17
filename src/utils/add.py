c713043dfda2fd278c5c4009d7f1db32204f529a
Sudhanshu Panda <spanda4@hawk.iit.edu>
2024-02-16 18:19:07 -0600

    add logic + aes logic

---
+++ src/utils/add.py
@@ -0,0 +1,33 @@
+from getpass import getpass
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA512
+from Crypto.Random import get_random_bytes
+from utils.dbconfig import dbconfig
+from rich import print as printc
+
+import utils.aesutil
+
+def computeMasterKey(mp, ds):
+    password = mp.encode()
+    salt = ds.encode()
+    key = PBKDF2(password, salt, 32, count=100000, hmac_hash_module=SHA512)
+    return key
+
+
+def addEntry(mp, ds, sitename, siteurl, email, username):
+#get the password
+    password = getpass("Password: ")
+
+    mk = computeMasterKey(mp, ds)
+
+    encrypted = utils.aesutil.encrpt(key=mk, source=password, KeyType="bytes")
+
+#Add to db
+db = dbconfig()
+cursor =db.cursor()
+query = "INSERT INTO pm.entries (sitename, siteurl, email, username, password) values (%s, %s, %s, %s, %s)"
+val = (sitename, siteurl, email, username, encrypted)
+cursor.execute(query, val)
+db.commit()
+
+printc("[green][+][/green] Added entry")
